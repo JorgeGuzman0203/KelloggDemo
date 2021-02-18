a98ed50493121d3d20e554fc0827c0df7f194385
Jorge Guzmán <jorge.guzman@igeckoo.com>
2021-02-18 16:04:10 -0600

    Test DB

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var conexion = mysql.createConnection({
 	host:'kelloggdemo.c370jneuet7d.us-east-2.rds.amazonaws.com',
 	user:'admin',
 	password:'Frusciante3*',
-	database:'Kellogg_Demo'
+	database:'Kellogg_Demo',
+	port:'3306'
 });
 
 conexion.connect(function(error){
